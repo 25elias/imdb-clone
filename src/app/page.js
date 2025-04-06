2fa29392e2fa131bee8cbd3d520f9e9472e95dc3
25elias <eliasusman83@gmail.com>
2025-04-06 12:31:38 +0300

    Updates on tmdb

--- src/app/page.js
+++ src/app/page.js
@@ -5,21 +5,20 @@ const API_KEY = process.env.APY_KEY;
 
 export default async function Home({searchParams}) {
   const genre = searchParams.genre || 'fetchTrending';
-  // const res = await fetch (
-  //   `https://api.themoveidb.org/3${
-  //     genre === 'fetchTopRated' ? `/move/top_rated` : `trending/all/week`
-  //   }?api_key=${API_KEY}&language=en-US&page=1`
-  // )
-  // const data = await res.json();
-  // if (!res.ok) {
-  //   throw new Error('Failed to fetch data. Please try Again.')
-  // }
-  // const results = data.results;
-  //results={results} 
+  const res = await fetch (
+    `https://api.themoveidb.org/3${genre === 'fetchTopRated' ? `/move/top_rated` : `trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`
+  )
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data. Please try Again.')
+  }
+  const results = data.results;
+  console.log(results);
+ 
   return (
     <div className="min-h-screen">
       
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,12 +2,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { FiThumbsUp } from "react-icons/fi";
 
-//
+//`https://image.tmbd.org/t/p/original/${result.backdrop_path || result.poster_path}`
 
 export default function Cards({result}) {
   return (
     <div className='group sm:mt-0 w-min sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 '>
-        <Link href={`/movie/${result.id}`}>
+        <Link href={`/movie/${id}`}>
             <Image
             src={`https://image.tmbd.org/t/p/original/${result.backdrop_path || result.poster_path}`}
             width={500}
@@ -21,11 +21,9 @@ export default function Cards({result}) {
                 <p className='line-clamp-2 text-md' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia impedit dicta autem provident minus saepe in a</p>
                 <h2 className='text-lg font-bold truncate'>Lorem Ipsum</h2>
                 <p>
-                    {
-                        {result.release_date || result.first_air_date}
-                        {<FiThumbsUp className='h-5 mr-1 ml-3' />}
-                        {result.vote_count}
-                    }
+                    {result.release_date || result.first_air_date}
+                    <FiThumbsUp className='h-5 mr-1 ml-3' />
+                    {result.vote_count}
                 </p>
             </div>
         </Link>
