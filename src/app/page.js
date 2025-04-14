815aa42364923c7866cf482334d46ba7050eea64
25elias <eliasusman83@gmail.com>
2025-04-14 11:38:25 +0300

    fixs on fetching.

--- src/app/page.js
+++ src/app/page.js
@@ -5,15 +5,13 @@ const API_KEY = process.env.APY_KEY;
 
 export default async function Home({searchParams}) {
   const genre = searchParams.genre || 'fetchTrending';
-  const res = await fetch(
-    `https://api.themoveidb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`
-  )
+  const res = await fetch(`https://api.rmdb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&laguage=en-US&page=1`)
   const data = await res.json();
   if (!res.ok) {
     throw new Error('Failed to fetch data. Please try Again.')
   }
   const results = data.results;
-  // console.log(results);
+  console.log(results);
  
   return (
     <div className="min-h-screen">
